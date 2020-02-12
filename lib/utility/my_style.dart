36a69d62b191496e9bd853c8a40b901bb9af9586
banana20 <bs@yathai.co.th>
2020-02-12 15:05:35 +0700

    Design UI Authen Success

--- lib/utility/my_style.dart
+++ lib/utility/my_style.dart
@@ -3,9 +3,9 @@ import 'package:flutter/material.dart';
 
 class Mystyle{
 // Field
-Color  textColor = Colors.purple.shade700;
-
-
+Color textColor = Color.fromARGB(0xff, 0xC0, 0xCa, 0x33);
+Color  mainColor=Color.fromARGB(0xff, 0x8c, 0x99, 0x00);
+Color  barColor=Color.fromARGB(0xff, 0xf5, 0xfd, 0x67);
 // Method
 Mystyle();
 
