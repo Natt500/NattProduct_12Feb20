3a3e90259f50df2c8f9a80ff00f877890f093e4b
banana20 <bs@yathai.co.th>
2020-02-13 11:05:04 +0700

    Get Value From Textfield Success

--- lib/widget/register.dart
+++ lib/widget/register.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:nattproduct/utility/my_style.dart';
+import 'package:nattproduct/utility/normal_dialog.dart';
 
 class Register extends StatefulWidget {
   @override
@@ -8,12 +9,16 @@ class Register extends StatefulWidget {
 
 class _RegisterState extends State<Register> {
   //Field
+  String name, user, password, rePassword;
 
   //Method
 
   Widget nameForm() {
-    Color color = Colors.purple;
+    Color color = Colors.blue.shade700;
     return TextField(
+      onChanged: (String string) {
+        name = string.trim();
+      },
       decoration: InputDecoration(
         enabledBorder: UnderlineInputBorder(
           borderSide: BorderSide(color: color),
@@ -31,11 +36,113 @@ class _RegisterState extends State<Register> {
     );
   }
 
+  Widget emailForm() {
+    Color color = Colors.green[700];
+    return TextField(
+      onChanged: (String string) {
+        user = string.trim();
+      },
+      decoration: InputDecoration(
+        enabledBorder: UnderlineInputBorder(
+          borderSide: BorderSide(color: color),
+        ),
+        helperStyle: TextStyle(color: color),
+        /* helperText: 'xxx@mail.com',*/
+        labelStyle: TextStyle(color: color),
+        labelText: 'Email :',
+        icon: Icon(
+          Icons.mail,
+          size: 36.0,
+          color: color,
+        ),
+      ),
+    );
+  }
+
+  Widget pwdForm() {
+    Color color = Colors.orange.shade700;
+    return TextField(obscureText: true, // type password ****
+      onChanged: (String string) {
+        password = string.trim();
+      },
+      decoration: InputDecoration(
+        enabledBorder: UnderlineInputBorder(
+          borderSide: BorderSide(color: color),
+        ),
+        helperStyle: TextStyle(color: color),
+
+        /*helperText: 'xxx@mail.com',*/
+        labelStyle: TextStyle(color: color),
+        labelText: 'Password :',
+        icon: Icon(
+          Icons.vpn_key,
+          size: 36.0,
+          color: color,
+        ),
+      ),
+      
+      
+    );
+  }
+
+  Widget singUpButton() {
+    return RaisedButton(
+      color: Mystyle().textColor,
+      child: Text(
+        'SignUp',
+        style: TextStyle(color: Colors.white),
+      ),
+      onPressed: () {},
+    );
+  }
+
+  Widget confirmpwdForm() {
+    Color color = Colors.purple;
+    return TextField(
+      onChanged: (String string) {
+        rePassword = string.trim();
+      },
+      decoration: InputDecoration(
+        enabledBorder: UnderlineInputBorder(
+          borderSide: BorderSide(color: color),
+        ),
+        helperStyle: TextStyle(color: color),
+        /* helperText: 'xxx@mail.com',*/
+        labelStyle: TextStyle(color: color),
+        labelText: 'Confirm Password :',
+        icon: Icon(
+          Icons.vpn_key,
+          size: 36.0,
+          color: color,
+        ),
+      ),
+      obscureText: true, // type password ****
+    );
+  }
+
   Widget registerButton() {
     return IconButton(
       tooltip: 'Upload To Server',
       icon: Icon(Icons.cloud_upload),
-      onPressed: () {},
+      onPressed: () {
+        print(
+            'name =$name, user= $user, password= $password, rePassword= $rePassword');
+
+        if (name == null ||
+            name.isEmpty ||
+            user == null ||
+            user.isEmpty ||
+            password == null ||
+            password.isEmpty ||
+            rePassword == null ||
+            rePassword.isEmpty) {
+          normalDialog(context, 'Have Space', 'Please Fill Every Blank');
+        }else if (password == rePassword){
+
+        } else{
+          normalDialog(context, 'Password Not Math', 'Please type Password Math Re-Password');
+        }
+      },
     );
   }
 
@@ -46,6 +153,9 @@ class _RegisterState extends State<Register> {
         padding: EdgeInsets.all(30.0),
         children: <Widget>[
           nameForm(),
+          emailForm(),
+          pwdForm(),
+          confirmpwdForm(),
         ],
       ),
       appBar: AppBar(
